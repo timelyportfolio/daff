9d77a804205e649765805e4f012624486976dabb
timelyportfolio <kent.russell@timelyportfolio.com>
2015-08-18 22:11:22 -0500

    change render_diff to use htmltools

--- R/render-diff.R
+++ R/render-diff.R
@@ -11,24 +11,50 @@
 #' @param fragment if \code{FALSE} the generated HTML will be a valid HTML document, otherwise it is a HTML fragment
 #' @param pretty if \code{TRUE} use fancy utf8 characters to make arrows prettier.
 #' @return generated html
-#'@export
+#'
+#' @importFrom htmltools HTML html_print attachDependencies htmlDependency
+#' @export
 render_diff <- function( diff
-                       , file=tempfile(fileext = ".html")
+                       , file=NULL
                        , view=interactive()
-                       , fragment=FALSE
+                       , fragment=TRUE
                        , pretty=FALSE
                        ){
   ctx <- diff$ctx
+  # keep fragment = TRUE despite parameter
+  #   we'll add the css and html wrapper with htmltools::save_html
   html <- ctx$call("render_diff", JS(diff$var_name), fragment, pretty)
-  cat(html, file = file)
-  if (view && file != "" && interactive()){
-    viewer <- getOption("viewer")
-    if (!is.null(viewer) && is.function(viewer)){
-      viewer(file)
-    }else{
-      utils::browseURL(file)
-    }
+
+  # allow for both view and save file
+  #   if fragment then leave as is
+  if(!is.null(file) ) {
+    cat(html, file = file)
   }
-  invisible(html)
+
+  # now get in htmltools HTML form for rmarkdown or Shiny
+  #   unfortunately requires us to rerun if fragment before
+  #   could avoid by always calling above with fragment = FALSE
+  #   and manually piecing together the rest of the HTML
+  if( !fragment ){
+    html <- HTML( ctx$call("render_diff", JS(diff$var_name), TRUE, pretty) )
+    # attach dependency for the CSS
+    html <- attachDependencies(
+      html
+      , htmlDependency(
+        name = "daff"
+        , version = "1.2.20"
+        , src = c( file = (function(){system.file( "css", package = "daff")})()  )
+        , stylesheet = "daff.css"
+      )
+    )
+    html <- htmltools::tags$div(class = "highlighter", html)
+  } else {
+    html <- HTML( html )
+  }
+
+  if( view && interactive() ) html_print( html )
+
+  html
+
 }
 
